9ae190cb21779dffbb633812b481519e17b18877
Ricardo <ricoledan@gmail.com>
2023-03-28 23:48:12 -0400

    remove config file

--- src/config/flipsidecrypto.ts
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const Flipside = require("@flipsidecrypto/sdk")
-
-exports.flipside = new Flipside(
-    process.env.FLIPSIDE_API_KEY,
-    "https://node-api.flipsidecrypto.com"
-);
-
-
